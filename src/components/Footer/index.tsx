2de7fa865be2d6f65b0a4f017739b3fff829a15e
Marcelo <27177081+Smart624@users.noreply.github.com>
2023-11-03 00:20:49 -0300

    Improved the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import { Box, HStack, VStack, Text } from '@chakra-ui/react';
+import { Box, HStack, VStack, Text, Link, Icon } from '@chakra-ui/react';
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
 const Footer: React.FC = () => (
-    <Box as="footer" bg="gray.800" color="white" py={8}>
-        <HStack spacing={8} justify="space-between" maxW="container.lg" mx="auto" px={8}>
-            <VStack align="stretch">
-                {/* Links Column 1 */}
+    <Box as="footer" bg="gray.800" color="white" py={10}>
+        <HStack spacing={0} justify="space-around" maxW="container.xl" mx="auto" px={8} wrap="wrap">
+            {/* Useful Links */}
+            <VStack align="flex-start" spacing={2}>
+                <Text fontWeight="bold">Links Úteis:</Text>
+                <Link href="/faq">FAQ</Link>
+                <Link href="/blog" style={{ pointerEvents: 'none', opacity: 0.4 }}>Blog (futuro)</Link>
+                <Link href="/guides" style={{ pointerEvents: 'none', opacity: 0.4 }}>Guides & Tutorials (futuro)</Link>
+                <Link href="/termos-de-servico">Termos de Serviço</Link>
+                <Link href="/politica-de-privacidade">Política de Privacidade</Link>
             </VStack>
 
-            <VStack align="stretch">
-                {/* Links Column 2 */}
-            </VStack>
-
-            <VStack align="stretch" color="white">
+            {/* Contact Information */}
+            <VStack align="center" spacing={2}>
+                <Text fontWeight="bold">Informações de Contato:</Text>
                 <Text>Endereço: Cidade Nova 1, Rua Paul Harris, 333, CEP: 13334070</Text>
                 <Text>Email: contato@nishi.com</Text>
                 <Text>Telefone: (19)3875-3831</Text>
-                {/* Add social media icons and other links here */}
+            </VStack>
+
+            {/* Social Media Icons */}
+            <VStack align="flex-end" spacing={2}>
+                <Text fontWeight="bold">Mídias Sociais:</Text>
+                <HStack spacing={2}>
+                    <Link href="https://facebook.com" isExternal>
+                        <Icon as={FaFacebookF} w={5} h={5} />
+                    </Link>
+                    <Link href="https://twitter.com" isExternal>
+                        <Icon as={FaTwitter} w={5} h={5} />
+                    </Link>
+                    <Link href="https://instagram.com" isExternal>
+                        <Icon as={FaInstagram} w={5} h={5} />
+                    </Link>
+                    <Link href="https://linkedin.com" isExternal>
+                        <Icon as={FaLinkedinIn} w={5} h={5} />
+                    </Link>
+                </HStack>
             </VStack>
         </HStack>
 
-        <Text textAlign="center" fontSize="sm" py={8}>
+        <Text textAlign="center" fontSize="sm" mt={10}>
             © 2023 Nishi Eletrônica. Todos os direitos reservados.
         </Text>
     </Box>
