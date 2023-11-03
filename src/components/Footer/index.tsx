4427abf2159c42a0b9e2d4af614cb6e0add8b6b6
Marcelo <27177081+Smart624@users.noreply.github.com>
2023-11-03 19:45:53 -0300

    Improved the footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { Box, HStack, VStack, Text, Link, Icon } from '@chakra-ui/react';
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import {
+    Box, HStack, VStack, Text, Link, Icon, Popover, PopoverTrigger,
+    PopoverContent, PopoverArrow, PopoverCloseButton, PopoverHeader,
+    PopoverBody, Button
+} from '@chakra-ui/react';
+import {
+    FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn, FaWhatsapp, FaClock
+} from 'react-icons/fa';
 
 const Footer: React.FC = () => (
     <Box as="footer" bg="gray.800" color="white" py={10}>
@@ -9,8 +15,8 @@ const Footer: React.FC = () => (
             <VStack align="flex-start" spacing={2}>
                 <Text fontWeight="bold">Links Úteis:</Text>
                 <Link href="/faq">FAQ</Link>
-                <Link href="/blog" style={{ pointerEvents: 'none', opacity: 0.4 }}>Blog (futuro)</Link>
-                <Link href="/guides" style={{ pointerEvents: 'none', opacity: 0.4 }}>Guides & Tutorials (futuro)</Link>
+                <Link href="/blog">Blog</Link>
+                <Link href="/guides">Guides & Tutorials</Link>
                 <Link href="/termos-de-servico">Termos de Serviço</Link>
                 <Link href="/politica-de-privacidade">Política de Privacidade</Link>
             </VStack>
@@ -21,10 +27,17 @@ const Footer: React.FC = () => (
                 <Text>Endereço: Cidade Nova 1, Rua Paul Harris, 333, CEP: 13334070</Text>
                 <Text>Email: contato@nishi.com</Text>
                 <Text>Telefone: (19)3875-3831</Text>
+                {/* WhatsApp Contact */}
+                <HStack spacing={2} pt={2}>
+                    <Icon as={FaWhatsapp} w={5} h={5} color="green" />
+                    <Link href="https://wa.me/551938753831" isExternal>
+                        <Text>Converse Conosco (WhatsApp)</Text>
+                    </Link>
+                </HStack>
             </VStack>
 
-            {/* Social Media Icons */}
-            <VStack align="flex-end" spacing={2}>
+            {/* Social Media Icons and Business Hours Popover */}
+            <VStack align="flex-end" spacing={4}>
                 <Text fontWeight="bold">Mídias Sociais:</Text>
                 <HStack spacing={2}>
                     <Link href="https://facebook.com" isExternal>
@@ -39,14 +52,29 @@ const Footer: React.FC = () => (
                     <Link href="https://linkedin.com" isExternal>
                         <Icon as={FaLinkedinIn} w={5} h={5} />
                     </Link>
-                </HStack>
-            </VStack>
         </HStack>
-
-        <Text textAlign="center" fontSize="sm" mt={10}>
-            © 2023 Nishi Eletrônica. Todos os direitos reservados.
-        </Text>
-    </Box>
+        <Popover trigger="hover" placement="top-start">
+            <PopoverTrigger>
+                <Button size="sm" leftIcon={<FaClock />} variant="ghost" colorScheme="whiteAlpha">
+                    Horários de Funcionamento
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent color="white" bg="gray.700" borderColor="gray.600">
+                <PopoverArrow bg="gray.700" />
+                <PopoverCloseButton />
+                <PopoverHeader>Horários de Funcionamento:</PopoverHeader>
+                <PopoverBody>
+                    <Text>Segunda a Sexta: 09:00–12:00, 13:00–17:00</Text>
+                    <Text>Sábado: 09:00–12:00</Text>
+                </PopoverBody>
+            </PopoverContent>
+        </Popover>
+    </VStack>
+</HStack>
+<Text textAlign="center" fontSize="sm" mt={10}>
+    © 2023 Nishi Eletrônica. Todos os direitos reservados.
+</Text>
+</Box>
 );
 
 export default Footer;
