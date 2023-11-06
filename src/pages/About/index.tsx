64527e6b119ce768c8517f2d7dc68419a930f124
Marcelo <27177081+Smart624@users.noreply.github.com>
2023-11-05 21:53:51 -0300

    Improved the about and contact pages

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,18 +1,48 @@
 import React from 'react';
-import {Center, Text} from '@chakra-ui/react';
-import '../../index.css';
+import {
+    Box,
+    Center,
+    VStack,
+    Heading,
+    Text,
+    Divider
+} from '@chakra-ui/react';
 import Layout from '../../components/Layout';
 
-
-const About: React.FC = () => (
-    <Layout>
-        <Center flexDir="column" p={8}>
-            <Text>
-                História da empresa, missão, visão e parcerias.
-                {/* Conteúdo sobre a empresa */}
-            </Text>
-        </Center>
-    </Layout>
-);
+const About: React.FC = () => {
+    return (
+        <Layout>
+            <Center flexDir="column" p={8}>
+                <VStack spacing={5}>
+                    <Heading as="h1" size="xl">Sobre Nós</Heading>
+                    <Box textAlign="center">
+                        <Text fontSize="xl" fontWeight="semibold">
+                            Pioneiros em Assistência Técnica em Indaiatuba desde 2000
+                        </Text>
+                        <Text my={3}>
+                            Fundada em 10 de fevereiro de 2000, a nossa assistência técnica é especializada
+                            no reparo e manutenção de televisores e microondas. Com mais de duas décadas de
+                            experiência, nos consolidamos como a maior referência no setor em Indaiatuba.
+                        </Text>
+                        <Divider my={5} />
+                        <Text fontSize="lg">
+                            Nossa Missão: Prover soluções ágeis e eficazes para prolongar a vida útil dos
+                            seus aparelhos, utilizando as técnicas mais avançadas e peças de alta qualidade.
+                        </Text>
+                        <Text my={3} fontSize="lg">
+                            Nossa Visão: Ser reconhecidos como a melhor e mais confiável assistência técnica de
+                            TVs e microondas, expandindo nossa expertise para novas regiões e tecnologias.
+                        </Text>
+                        <Text fontSize="lg">
+                            Valorizamos parcerias duradouras e confiamos na qualidade do nosso trabalho,
+                            por isso colaboramos com as principais marcas do mercado para garantir a
+                            melhor assistência para nossos clientes.
+                        </Text>
+                    </Box>
+                </VStack>
+            </Center>
+        </Layout>
+    );
+};
 
 export default About;
--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,21 +1,73 @@
 import React from 'react';
-import { Center, FormLabel, Input, Button, VStack } from '@chakra-ui/react';
-import Layout from "../../components/Layout";
+import { useForm } from 'react-hook-form';
+import {
+    Center,
+    FormControl,
+    FormLabel,
+    Input,
+    Button,
+    VStack,
+    Textarea,
+    Text,
+    Divider,
+    Heading,
+} from '@chakra-ui/react';
+import Layout from '../../components/Layout';
+import axios from 'axios';
 
-const Contact: React.FC = () => (
-    <Layout>
-        <Center flexDir="column" p={8}>
-            <VStack spacing={4} width="md">
-                <FormLabel>Nome</FormLabel>
-                <Input placeholder="Seu nome" />
-                <FormLabel>Email</FormLabel>
-                <Input placeholder="Seu email" />
-                <FormLabel>Mensagem</FormLabel>
-                <Input placeholder="Sua mensagem" />
-                <Button colorScheme="blue">Enviar</Button>
-            </VStack>
-        </Center>
-</Layout>
-);
+type FormData = {
+    name: string;
+    email: string;
+    message: string;
+};
+
+const Contact: React.FC = () => {
+    const { register, handleSubmit, reset } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        axios.post('/api/contact', data)
+            .then(response => {
+                console.log(response);
+                reset();
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    return (
+        <Layout>
+            <Center flexDir="column" p={8}>
+                <VStack spacing={4} width="full" maxW="md">
+                    <Heading>Contato</Heading>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <FormControl>
+                            <FormLabel>Nome</FormLabel>
+                            <Input placeholder="Seu nome" {...register('name')} />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Email</FormLabel>
+                            <Input placeholder="Seu email" type="email" {...register('email')} />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Mensagem</FormLabel>
+                            <Textarea placeholder="Sua mensagem" {...register('message')} rows={5} /> {/* Modificado aqui */}
+                        </FormControl>
+                        <Button colorScheme="blue" type="submit">Enviar</Button>
+                    </form>
+                    <Divider my={6} />
+                    <Text fontWeight="bold">Horário de Funcionamento:</Text>
+                    <Text>Segunda a Sexta: 09:00–12:00, 13:00–17:00</Text>
+                    <Text>Sábado: 09:00–12:00</Text>
+                    <iframe
+                        width="100%"
+                        height="350"
+                        src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=Rua%20Paul%20Harris,%20333+(My%20Business%20Name)&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+                    />
+                </VStack>
+            </Center>
+        </Layout>
+    );
+};
 
 export default Contact;
